12b7d79a8f16f831319311dcdb2f351ddd7f34a0
Sai Deepak <deepak@Sais-MacBook-Air.local>
2026-06-01 22:37:18 +0530

    Add .gitignore and remove build/test artifacts from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/deepak/Desktop/clgg/PROJECTS/order_book_engine
-# Build directory: /Users/deepak/Desktop/clgg/PROJECTS/order_book_engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(OrderBookTests "/Users/deepak/Desktop/clgg/PROJECTS/order_book_engine/build/run_tests")
-set_tests_properties(OrderBookTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/deepak/Desktop/clgg/PROJECTS/order_book_engine/CMakeLists.txt;34;add_test;/Users/deepak/Desktop/clgg/PROJECTS/order_book_engine/CMakeLists.txt;0;")
